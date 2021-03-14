ada3a99de84cc0542a4a4ec14a26d779754fb5ba
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-14 11:25:57 -0600

    Se agrego un nuevo widget personalizado.

--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,2 +1,3 @@
 export 'package:shoes_app/src/widgets/custom_appbar_widget.dart';
 export 'package:shoes_app/src/widgets/shoe_size_preview_widget.dart';
+export 'package:shoes_app/src/widgets/shoe_description.dart';
