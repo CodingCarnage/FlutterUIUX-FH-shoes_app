7b43a1cb47c2517db57fbf1c9946fd7991463e49
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-13 20:22:35 -0600

    Se creo un menu para seleccionar la talla y de acuerdo a la talla seleccionada cambia sus propiedades/colores.

--- lib/src/widgets/shoe_size_preview_widget.dart
+++ lib/src/widgets/shoe_size_preview_widget.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'package:shoes_app/src/models/shoe_model.dart';
+
 class ShoeSizePreview extends StatelessWidget {
   const ShoeSizePreview({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _ShoeSizePreviewBackground();
+  }
+}
+
+class _ShoeSizePreviewBackground extends StatelessWidget {
+  const _ShoeSizePreviewBackground({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +24,35 @@ class ShoeSizePreview extends StatelessWidget {
         width: double.infinity,
         height: 425.0,
         decoration: BoxDecoration(
+          //* Change to main color.
           color: Color(0xffFFD54F),
           borderRadius: BorderRadius.circular(50.0),
         ),
-        child: Column(
-          children: <Widget>[
-            _ShoeWithShadow(),
-          ],
-        ),
+        child: _ShoeSizePreviewForeground(),
       ),
     );
   }
 }
 
-class _ShoeWithShadow extends StatelessWidget {
-  const _ShoeWithShadow({Key key}) : super(key: key);
+class _ShoeSizePreviewForeground extends StatelessWidget {
+  const _ShoeSizePreviewForeground({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _Shoe(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: _ShoeSizeSelection(),
+        ),
+      ],
+    );
+  }
+}
+
+class _Shoe extends StatelessWidget {
+  const _Shoe({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +65,9 @@ class _ShoeWithShadow extends StatelessWidget {
             right: 0.0,
             child: _ShoeShadow(),
           ),
-          Image(image: AssetImage('assets/images/azul.png')),
+          Image(
+            image: AssetImage('assets/images/azul.png'),
+          ),
         ],
       ),
     );
@@ -46,9 +75,7 @@ class _ShoeWithShadow extends StatelessWidget {
 }
 
 class _ShoeShadow extends StatelessWidget {
-  const _ShoeShadow({
-    Key key,
-  }) : super(key: key);
+  const _ShoeShadow({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -61,9 +88,77 @@ class _ShoeShadow extends StatelessWidget {
           borderRadius: BorderRadius.circular(100.0),
           boxShadow: [
             BoxShadow(
-              color: Color(0xffF79E38),
+              color: Color(0xffF79E38), //* Change to main color.
               blurRadius: 40.0,
-            )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ShoeSizeSelection extends StatelessWidget {
+  const _ShoeSizeSelection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        _ShoeSizeButton(7),
+        _ShoeSizeButton(7.5),
+        _ShoeSizeButton(8),
+        _ShoeSizeButton(8.5),
+        _ShoeSizeButton(9),
+        _ShoeSizeButton(9.5),
+      ],
+    );
+  }
+}
+
+class _ShoeSizeButton extends StatelessWidget {
+  const _ShoeSizeButton(
+    this.shoeSize, {
+    Key key,
+  }) : super(key: key);
+
+  final double shoeSize;
+
+  @override
+  Widget build(BuildContext context) {
+    final ShoeModel shoeModel = Provider.of<ShoeModel>(context);
+    
+    return GestureDetector(
+      onTap: () {
+        final ShoeModel shoeModel = Provider.of<ShoeModel>(context, listen: false);
+        shoeModel.shoeSize = this.shoeSize;
+      },
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(
+          '${shoeSize.toString().replaceAll('.0', '')}',
+          style: TextStyle(
+            //* Change to main color.
+            color: (this.shoeSize == shoeModel.shoeSize) ? Colors.white : Color(0xffF1A23A),
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        width: 42.5,
+        height: 42.5,
+        decoration: BoxDecoration(
+          //* Change color according to main color.
+          color: (this.shoeSize == shoeModel.shoeSize) ? Color(0xffF1A23A) : Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: <BoxShadow>[
+            if (this.shoeSize == shoeModel.shoeSize)
+              BoxShadow(
+                //* Change to main color.
+                color: Color(0xffF1A23A),
+                blurRadius: 10.0,
+                offset: Offset(0, 5),
+              ),
           ],
         ),
       ),
