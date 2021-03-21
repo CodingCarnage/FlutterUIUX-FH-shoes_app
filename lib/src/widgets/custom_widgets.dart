db97a1dd16f95bd426ea8c276830279bf8c711c6
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-21 15:40:48 -0600

    Se agrego nuevos customizados.

--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,3 +1,5 @@
 export 'package:shoes_app/src/widgets/custom_appbar_widget.dart';
 export 'package:shoes_app/src/widgets/shoe_size_preview_widget.dart';
 export 'package:shoes_app/src/widgets/shoe_description.dart';
+export 'package:shoes_app/src/widgets/add_to_cart.dart';
+export 'package:shoes_app/src/widgets/custom_button.dart';
