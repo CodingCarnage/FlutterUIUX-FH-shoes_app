14697104a0b119696a51e3e5107f3c96298cd767
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-21 16:09:32 -0600

    Se agrego una nueva propiedad al modelo.

--- lib/src/models/shoe_model.dart
+++ lib/src/models/shoe_model.dart
@@ -8,6 +8,14 @@ class ShoeModel with ChangeNotifier {
   set shoeSizes(List<double> shoeSizes) {
     this._shoeSizes = shoeSizes;
   }
+
+  bool _fullScreen;
+
+  bool get fullScreen => this._fullScreen;
+
+  set fullScreen(bool fullScreen) {
+    this._fullScreen = fullScreen;
+  }
   
   String _assetsImage = 'assets/images/azul.png';
 
