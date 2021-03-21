336168dc4961f35a69fff64456cb2182abf110e5
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-21 17:29:15 -0600

    Se agrego un selector de color y botones.

--- lib/src/pages/shoe_detail_page.dart
+++ lib/src/pages/shoe_detail_page.dart
@@ -24,12 +24,71 @@ class ShoeDetailPage extends StatelessWidget {
                     description:
                         'The Nike Air Max 720 goes bigger than ever before with Nike\'s taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so. The Nike Air Max 720 goes bigger than ever before with Nike\'s taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so. The Nike Air Max 720 goes bigger than ever before with Nike\'s taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so. The Nike Air Max 720 goes bigger than ever before with Nike\'s taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.',
                   ),
-                  _AmountBuyNow()
+                  _AmountBuyNow(),
+                  _ColorSelectionAndMore(),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _ColorSelectionAndMore extends StatelessWidget {
+  const _ColorSelectionAndMore({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  Positioned(left: 90, child: _ShoeColor(color: Color(0xffC6D642))),
+                  Positioned(left: 60, child: _ShoeColor(color: Color(0xffFFAD29))),
+                  Positioned(left: 30, child: _ShoeColor(color: Color(0xff2099F1))),
+                  _ShoeColor(color: Color(0xff364D56)),
+                ],
+              ),
+            ),
+            CustomButton(
+              text: 'More related items',
+              height: 32.5,
+              width: 170.0,
+              color: Colors.orangeAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ShoeColor extends StatelessWidget {
+  const _ShoeColor({
+    Key key,
+    @required this.color,
+  }) : super(key: key);
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 45.0,
+      height: 45.0,
+      decoration: BoxDecoration(
+        color: this.color,
+        shape: BoxShape.circle,
       ),
     );
   }
