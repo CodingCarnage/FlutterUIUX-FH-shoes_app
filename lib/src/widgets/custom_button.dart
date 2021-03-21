9883417ff0281c407af755a842950a3af729b5f4
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-03-21 16:29:17 -0600

    Se agregaron nuevas propiedades al widget.

--- lib/src/widgets/custom_button.dart
+++ lib/src/widgets/custom_button.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key key, @required this.text}) : super(key: key);
+  const CustomButton({
+    Key key,
+    @required this.text,
+    this.height = 50.0,
+    this.width = 150.0,
+    this.color = Colors.blueAccent
+  }) : super(key: key);
 
   final String text;
+  final double height;
+  final double width;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 150.0,
-      height: 50.0,
+      width: this.width,
+      height: this.height,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100.0),
-        color: Colors.amberAccent,
+        color: this.color,
       ),
       child: Text(
         '$text',
